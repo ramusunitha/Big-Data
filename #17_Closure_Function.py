eeb64dc913c4cdcc0d9a4f01e2024583a3b9d843
Sunitha Ramu <66831341+ramusunitha@users.noreply.github.com>
2022-06-26 13:34:40 +0530

    Add files via upload

---
+++ #17_Closure_Function.py
@@ -0,0 +1,9 @@
+#Closure Function
+print("\nStarting the Program")
+counter=0
+counter+=1
+print("Closure Scenario {}".format(counter))
+a=100
+def x(b):
+    return a+b
+print(x(10))
---
+++ #18_Class_Closure.py
@@ -0,0 +1,8 @@
+#Class - Closure Function
+class cls1():
+    a=100
+    def x(self,b):
+        return self.a+b
+
+obj1=cls1()
+print(obj1.x(10))
